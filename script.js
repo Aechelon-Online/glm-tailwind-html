40d710180d0cbf8ed2a44c941695a5e30331bb7b
Aechelon-Online <info@aechelon.online>
2023-06-03 14:02:41 -0500

    Fixed JS for select arrows

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const dropDown = document.querySelector(".menu")
 const toggleIcon = document.querySelector(".toggle-icon")
 const eventSelect = document.querySelector(".event-select")
+const eventArrow = document.querySelector(".event-arrow")
 const serviceSelect = document.querySelector(".service-select")
+const selectArrow = document.querySelector(".select-arrow")
 const radios = document.querySelector(".event-select")
 const list = document.querySelector(".list")
 const dots = document.querySelector(".selections")
 const checkboxes = document.querySelector(".entertainment-checkboxes")
+const arrowEvent = document.querySelector(".arrow-click")
 
 let isOpen = false
 
@@ -20,7 +23,7 @@ let isDropped = true
 checkboxes.addEventListener('click', () => {
     list.classList.toggle("hidden")
     isDropped = !isDropped
-    serviceSelect.innerText = isDropped ? "▼" : "✕"
+    selectArrow.innerText = isDropped ? "▼" : "✕"
     if(serviceSelect.innerText === "✕"){
         serviceSelect.style.margin = "-2px 1px"
     } else {
@@ -31,13 +34,23 @@ checkboxes.addEventListener('click', () => {
 radios.addEventListener('click', () => {
     dots.classList.toggle("hidden")
     isDropped = !isDropped
-    eventSelect.innerText = isDropped ? "▼" : "✕"
+    eventArrow.innerText = isDropped ? "▼" : "✕"
     if(eventSelect.innerText === "✕"){
         eventSelect.style.margin = "-2px 1px"
     } else {
         eventSelect.style.margin = "0"
     }
-   
+})
+
+arrowEvent.addEventListener('click', () => {
+  dots.classList.toggle("hidden")
+  isDropped = !isDropped
+  eventArrow.innerText = isDropped ? "▼" : "✕"
+  if(eventSelect.innerText === "✕"){
+      eventSelect.style.margin = "-2px 1px"
+  } else {
+      eventSelect.style.margin = "0"
+  }
 })
 
 // Calendar API JS
