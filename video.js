4b60b83cf0b80e754c2fb9f797dfc74de8655006
Aechelon-Online <info@aechelon.online>
2023-08-14 12:01:03 -0500

    Added new pages for Grapevine and Mansfield and added links

--- video.js
+++ video.js
@@ -5,16 +5,17 @@ const videos = {
     'https://www.youtube.com/embed/E5Ah_hqbzIg': 'Greg Luce Music - Promo Video',
     'https://www.youtube.com/embed/C0FUuuClppY': 'Greg Luce Music - Short Live Promo',
     'https://www.youtube.com/embed/b7fEKZ8TE1E': 'Private Party - Listen To The Music',
-    'https://www.youtube.com/embed/n60Pm7AsEB0': 'Rocket Man - Prestonwood CC'
+    'https://www.youtube.com/embed/n60Pm7AsEB0': 'Rocket Man - Prestonwood CC',
+    'https://www.youtube.com/embed/AlJo1GmbsLc': 'Fancy Like - Wedding Groove'
     }
 
 let videoPlayer = ""
 for (let [url, title] of Object.entries(videos)) {
 
 videoPlayer += `
-    <div class="video-player mx-4">
-        <iframe width="560" height="315" src="${url}" frameborder="0" allowfullscreen class="video"></iframe>
-        <h4 class="text-3xl text-white">${title}</h4>
+    <div class="video-player">
+        <iframe src="${url}" allowfullscreen class="vid-frame p-2"></iframe>
+        <h4 class="text-md p-2 text-white">${title}</h4>
     </div>
     `
 }
