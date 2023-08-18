f9cfd28f8041a59ae9ab9a0718fe1b94a2824105
Aechelon-Online <info@aechelon.online>
2023-08-18 16:34:14 -0500

    Added more YT vids with href links

--- video.js
+++ video.js
@@ -4,7 +4,15 @@ const videos = [
     { link:'https://youtu.be/C0FUuuClppY', thumbnail: 'https://img.youtube.com/vi/C0FUuuClppY/mqdefault.jpg', title: 'Greg Luce Music - Short Live Promo'},
     { link:'https://youtu.be/-TO08B3AVlM', thumbnail: 'https://img.youtube.com/vi/b7fEKZ8TE1E/mqdefault.jpg', title:'Private Party - Listen To The Music'},
     { link:'https://youtu.be/qVtODE5EDXQ', thumbnail: 'https://img.youtube.com/vi/n60Pm7AsEB0/mqdefault.jpg', title:'Rocket Man - Prestonwood CC'},
-    { link:'https://youtu.be/AlJo1GmbsLc', thumbnail: 'https://img.youtube.com/vi/AlJo1GmbsLc/mqdefault.jpg', title:'Fancy Like - Wedding Groove'}
+    { link:'https://youtu.be/AlJo1GmbsLc', thumbnail: 'https://img.youtube.com/vi/AlJo1GmbsLc/mqdefault.jpg', title:'Fancy Like - Wedding Groove'},
+    { link:'https://youtu.be/aifT0B_-YmE', thumbnail: 'https://img.youtube.com/vi/aifT0B_-YmE/mqdefault.jpg', title:'Country Roads - Greg Luce Music'},
+    { link:'https://youtu.be/JK4-KuTJF1A', thumbnail: 'https://img.youtube.com/vi/JK4-KuTJF1A/mqdefault.jpg', title:'Dock Of The Bay - Greg Luce Music'},
+    { link:'https://youtu.be/U6ok9nhBydM', thumbnail: 'https://img.youtube.com/vi/U6ok9nhBydM/mqdefault.jpg', title:'Every Breath You Take - Greg Luce Music'},
+    { link:'https://youtu.be/7Bfv9C15WCo', thumbnail: 'https://img.youtube.com/vi/7Bfv9C15WCo/mqdefault.jpg', title:'Free Fallin - Greg Luce Music'},
+    { link:'https://youtu.be/FTaKbhlXLkc', thumbnail: 'https://img.youtube.com/vi/FTaKbhlXLkc/mqdefault.jpg', title:'Superstition - Greg Luce Music'},
+    { link:'https://youtu.be/rffcNo1dDpc', thumbnail: 'https://img.youtube.com/vi/rffcNo1dDpc/mqdefault.jpg', title:'Take It Easy - Greg Luce Music'},
+    { link:'https://youtu.be/gGYr5zs7sJM', thumbnail: 'https://img.youtube.com/vi/gGYr5zs7sJM/mqdefault.jpg', title:'Time Of Your Life - Greg Luce Music'},
+    { link:'https://youtu.be/PCARdM_Ds9E', thumbnail: 'https://img.youtube.com/vi/PCARdM_Ds9E/mqdefault.jpg', title:'Why Georgia - Greg Luce Music'}
 ]
 
 let videoPlayer = ""
