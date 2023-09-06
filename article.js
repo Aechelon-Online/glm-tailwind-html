af512dcf0d2932ee69d4fc6a181b64c4f0d7a114
Aechelon-Online <info@aechelon.online>
2023-09-06 12:14:49 -0500

    Resized blog pics to fit and put media breakpoints for images and text

--- article.js
+++ article.js
@@ -3,19 +3,21 @@ const articles = [
     { title: 'Event DJ Services: Elevate Your Occasion with Expert Entertainment', link:'https://greglucemusic.com/blog/event-dj-services-expert-entertainment/index.html', pic:'/images/event-dj-back.png'},
     { title: 'Emerging Party DJ Trends in 2023: Shaping the Sound of the Next Generation', link:'https://greglucemusic.com/blog/emerging-party-dj-trends-in-2023/index.html', pic:'/images/turntable-wavier-lights.png'},
     { title: 'Setting the Beat: Current Party DJ Industry Trends', link:'https://greglucemusic.com/blog/current-party-dj-industry-trends/index.html', pic:'/images/dj-mixing-at-party.png'},
-    { title: 'Elevating the Beat: Advanced Mixing Tips for High-Energy Party DJs', link:'https://greglucemusic.com/blog/advanced-mixing-tips-for-party-djs/index.html', pic:'/images/back-of-dj-hands-up.webp'}
+    { title: 'Elevating the Beat: Advanced Mixing Tips for High-Energy Party DJs', link:'https://greglucemusic.com/blog/advanced-mixing-tips-for-party-djs/index.html', pic:'/images/back-of-dj-hands-up.webp'},
+    { title: 'Using Mixing Techniques for Party DJs: Mastering Transitions', link:'https://greglucemusic.com/blog/mastering-transitions-for-party-djs/index.html', pic:'/images/mastering-transitions-party-dj.webp'},
+    { title: 'Smooth Party DJ Blending Methods', link:'https://greglucemusic.com/blog/smooth-party-dj-blending-methods/index.html', pic:'/images/dj-mixing-board.webp'}
 ]
 
 let blog = ""
 
 articles.forEach(function(article) {
     blog += `
-    <div class="flex items-center article-cell px-2 mx-2 border-2 border-black">
-        <div class="article-width">
-            <img src="${article.pic}" alt="event dj services" />
+    <div class="flex items-center px-2 mx-2 border-2 border-black">
+        <div class="w-[280px] h-[100px] tablet:w-[320px] tablet:h-[120px] desktop:w-[400px] desktop:h-[150px] overflow-y-hidden">
+            <img src="${article.pic}" class="object-fit" alt="event dj services" />
         </div>
         <div>
-        <h2 class="text-[0.75rem] desktop:text-[1.25rem] tablet:text-[1rem] text-center px-2"><a href="${article.link}">${article.title.slice(0,25) + "..."}</a></h2>
+        <h2 class="text-[0.75rem] tablet:text-[1rem] desktop:text-[1.25rem] text-center px-2"><a href="${article.link}">${article.title.slice(0,30) + "..."}</a></h2>
         </div>
     </div>
     `
