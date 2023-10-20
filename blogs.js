c25439e79ce1033a4fcf9d989f4a6cd0c95c41ba
Greg's MacBook Pro M1 <macuser@macUsers-MBP.attlocal.net>
2023-10-20 14:46:10 -0500

    New blogs and updated site

--- blogs.js
+++ blogs.js
@@ -1,5 +1,5 @@
 const blogs = [
-    {title: 'Elevating Private Events', link:'https://greglucemusic.com/blog/index.html'},
+    {title: 'Elevating Private Events', link:'https://greglucemusic.com/blog/index.html', pic:'/images/dj-solo-musician.webp'},
     {title: 'Event DJ Services', link:'https://greglucemusic.com/blog/event-dj-services-expert-entertainment/index.html'},
     {title: 'Emerging Trends in 2023', link:'https://greglucemusic.com/blog/emerging-party-dj-trends-in-2023/index.html'},
     {title: 'Setting The Beat', link:'https://greglucemusic.com/blog/current-party-dj-industry-trends/index.html'},
@@ -13,6 +13,7 @@ let page = ""
 blogs.forEach(function(blog) {
     page += `
     <div class="flex items-center px-2 mx-2">
+        <img style="width:200px;" src="${blog.pic}" /> 
         <h2 class="text-[0.75rem] tablet:text-[1rem] desktop:text-[1.25rem] text-center px-2"><a href="${blog.link}">${blog.title}</a></h2>
     </div>
     `
